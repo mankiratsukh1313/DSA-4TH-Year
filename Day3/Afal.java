0fd590624992dc4ea39f6636a1c807c5c1795103
Mankirat1313 <mankiratjessi@gmail.com>
2026-07-06 15:06:35 +0530

    Day3 LinkedList Implementation

---
+++ Day3/Afal.java
@@ -0,0 +1,60 @@
+package Day3;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
+    }
+}
+
+public class Afal {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.addFirst(20);
+        list.addFirst(10);
+        list.addLast(30);
+
+        list.display();
+    }
+}
